9d9da77fc3eda4d87b019a4100859ef3b37ccd64
AKA <akagithub@gmail.com>
2025-04-24 18:51:10 +0500

    lesson_03: static

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from mainapp.views import index, contacts
 
+app_name = 'mainapp'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('', include('mainapp.urls', namespace='mainapp')),
+    path('', index, name='index'),
+    path('contacts', contacts, name='contacts'),
 ]
