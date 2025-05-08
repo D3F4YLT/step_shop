b597539985a8882f3ca5b317cda6aa84779f7e73
AKA <akagithub@gmail.com>
2025-05-08 18:32:32 +0500

    lesson_06: Работа с данными

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -23,7 +23,7 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', index, name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('about', about, name='about'),
+    path('about/', about, name='about'),
     path('products/', products, name='products'),
     path('product/', product, name='product'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+from mainapp.models import Product
+
+
 def index(request):
     title = 'Главная'
 
+    prods = Product.objects.all()[:4]
+
     context = {
         'title': title,
+        'products': prods,
     }
 
 
