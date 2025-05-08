a92daea54a654cc2b9dfcf6a9d5825a9fd4a10db
AKA <akagithub@gmail.com>
2025-05-08 16:04:13 +0500

    домашка 2

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.urls import path
 
-from mainapp.views import index, contacts
+from mainapp.views import index, contacts, about, products, product
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('contacts', contacts, name='contacts'),
+    path('contacts/', contacts, name='contacts'),
+    path('about', about, name='about'),
+    path('products/', products, name='products'),
+    path('product/', product, name='product'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -11,4 +11,33 @@ def index(request):
     return render(request, 'index.html', context)
 
 def contacts(request):
+    title = 'Контакты'
+
+    context = {
+        'title': title,
+    }
     return render(request, 'contact.html')
+
+def about(request):
+    title = 'О нас'
+
+    context = {
+        'title': title,
+    }
+    return render(request, 'about.html')
+
+def products(request):
+    title = 'Продукты'
+
+    context = {
+        'title': title,
+    }
+    return render(request, 'products.html')
+
+def product(request):
+    title = 'Продукты'
+
+    context = {
+        'title': title,
+    }
+    return render(request, 'product.html')
