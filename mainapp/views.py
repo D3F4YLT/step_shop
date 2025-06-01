26594f48d865b65fba930a1527a17113b067be20
AKA <akagithub@gmail.com>
2025-06-01 15:39:04 +0500

    lesson_10

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from mainapp.models import Product
+from mainapp.models import Product, Category
 
 
 def index(request):
@@ -34,16 +34,23 @@ def about(request):
 
 def products(request):
     title = 'Продукты'
+    prods = Product.objects.all()
+    categories = Category.objects.all()
 
     context = {
         'title': title,
+        'products': prods,
+        'categories': categories,
     }
-    return render(request, 'products.html')
+    return render(request, 'products.html', context)
 
 def product(request):
     title = 'Продукты'
 
+    prod = Product.objects.get(id=1)
+
     context = {
         'title': title,
+        'product': prod,
     }
-    return render(request, 'product.html')
+    return render(request, 'product.html', context)
